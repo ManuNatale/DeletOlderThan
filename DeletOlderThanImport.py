164fa7ec5b390b28c6276a5e722db210059cdb14
ManuNatale <61042475+ManuNatale@users.noreply.github.com>
2020-02-17 14:21:17 +0100

    Update DeletOlderThanImport.py

--- DeletOlderThanImport.py
+++ DeletOlderThanImport.py
@@ -5,36 +5,41 @@ import shutil
 import argparse
 from datetime import datetime
 
-def oldDateCheck(path, folder, testTrueFalse, olderThan):
-    
-    now = time.time()
-    #print('heure actu ' + str(now))
 
-    fileStatsObj = os.stat(path)
-    #print(str(fileStatsObj))
-    creationTime = time.ctime ( fileStatsObj [ stat.ST_CTIME ] )
-    creationTimeSec =  ( fileStatsObj [ stat.ST_CTIME ] )
-    print('Modification date :' + creationTime + ', sec Unix : ' + str(creationTimeSec))
-    diff = now - creationTimeSec
-    print('Time difference beetweem now and modification date : ' + str(round(diff)))
-    if(diff > olderThan): 
-        if(testTrueFalse == False):
-            print('Old folder finded. Removing ==> {}\n'.format(folder))
-            try:
-                pass
-                #shutil.rmtree(path, ignore_errors=True)
-            except:
-                print('[{}] : Error removing folder : {} !!!'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), folder))
-            return True
+    
+# Main function  
+def FolderCleaning(path, daysToSec, testTrueFalse): # Use DeletOlderThanImport.FolderCleaning(path, daysToSec, test) # args = path, remove_folders_older_than (INT, in days), Test (True or False)
+
+    def oldDateCheck(path, folder, testTrueFalse, olderThan):
+    
+        now = time.time()
+        #print('heure actu ' + str(now))
+
+        fileStatsObj = os.stat(path)
+        #print(str(fileStatsObj))
+        creationTime = time.ctime ( fileStatsObj [ stat.ST_CTIME ] )
+        creationTimeSec =  ( fileStatsObj [ stat.ST_CTIME ] )
+        print('Modification date :' + creationTime + ', sec Unix : ' + str(creationTimeSec))
+        diff = now - creationTimeSec
+        print('Time difference beetweem now and modification date : ' + str(round(diff)))
+        if(diff > olderThan): 
+            if(testTrueFalse == False):
+                print('Old folder finded. Removing ==> {}\n'.format(folder))
+                try:
+                    pass
+                    #shutil.rmtree(path, ignore_errors=True)
+                except:
+                    print('[{}] : Error removing folder : {} !!!'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), folder))
+                return True
+            else:
+                print('Old folder finded ==> {}\nThis is a test, any folder has been removed.'.format(folder))
+                return True
         else:
-            print('Old folder finded ==> {}\nThis is a test, any folder has been removed.'.format(folder))
-            return True
-    else:
-        return False
-    
-    
-def FolderCleaning(path, olderThan, testTrueFalse):
-    print('\n[{}] : Cleaning session started ! Checking folders older than : {} day...'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), round((olderThan/60/60)/24, 2)))
+            return False
+
+    olderThan = ((daysToSec*60)*60)*24
+
+    print('\n[{}] : Cleaning session started ! Checking folders older than : {} day...'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), round(daysToSec, 2)))
     time.sleep(3)
     if(testTrueFalse == True):
         print('\n[{}] : Test session any folders are going to be removed'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
@@ -53,7 +58,7 @@ def FolderCleaning(path, olderThan, testTrueFalse):
         if(oldDateCheck(folder_path, i, testTrueFalse, olderThan) == True):
             delet_count += 1
     print('\n[{}] : Number of folders find : {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), Nb_Of_folders))
-    print('\n[{}] : Number of folders older than {} days : {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), round((olderThan/60/60)/24, 2), delet_count))
+    print('\n[{}] : Number of folders older than {} days : {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), round(daysToSec, 2), delet_count))
     if(testTrueFalse == True):
         print('\n[{}] : ||Test = True|| number of folders to delet : {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), delet_count))
     else:
