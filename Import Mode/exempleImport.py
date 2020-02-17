3c652244ab0c2a270def370820ca5a99fe70a7bc
ManuNatale <61042475+ManuNatale@users.noreply.github.com>
2020-02-17 14:38:11 +0100

    Update exempleImport.py

--- Import Mode/exempleImport.py
+++ Import Mode/exempleImport.py
@@ -1,7 +1,7 @@
-import DeletOlderThan_import
+import DeletOlderThan
 
 path = r'C:\Users\Desktop'
 days = 10
 test = True
 
-DeletOlderThan_import.FolderCleaning(path, days, test) # args = path, remove_folders_older_than (INT, in days), Test (True or False)
+DeletOlderThan.FolderCleaning(path, days, test) # args = path, remove_folders_older_than (INT, in days), Test (True or False)
