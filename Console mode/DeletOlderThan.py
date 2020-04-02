b8efe90c331500e41edf3a02df89938571076550
ManuNatale <61042475+ManuNatale@users.noreply.github.com>
2020-04-02 14:21:55 +0200

    Update DeletOlderThan.py

--- Console mode/DeletOlderThan.py
+++ Console mode/DeletOlderThan.py
@@ -46,7 +46,7 @@ def FolderCleaning(path, olderThan, testTrueFalse):
 
     for i in folders:
         time.sleep(0.01)
-        print('\n\nChecking floder : ' + i)
+        print('\n\nChecking folder : ' + i)
         folder_path = ('{}/{}'.format(path, i))
         print('Path = {}'.format(folder_path))
         if(oldDateCheck(folder_path, i, testTrueFalse, olderThan) == True):
